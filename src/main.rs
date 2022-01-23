226d6c4effdbbe6955eda00fe23f7df3c570758e
Matthias <26800596+not-matthias@users.noreply.github.com>
2022-01-23 16:36:07 +0100

    Updated the code a little bit (added new detection, moved to functions)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use core::arch::x86_64::__cpuid;
 use core::arch::x86_64::_rdtsc;
 use std::time::Duration;
+use winapi::um::processthreadsapi::{GetCurrentThread, SetThreadPriority};
+use winapi::um::winbase::THREAD_PRIORITY_TIME_CRITICAL;
 
-fn main() {
-    let print_values: bool = true;
-    const ITERATION_COUNT: u64 = 100;
-    const SLEEP_DURATION: u64 = 10;
+const PRINT_VALUES: bool = false;
+const ITERATION_COUNT: u64 = 20_000;
+const SLEEP_DURATION: u64 = 10;
 
+#[inline(always)]
+fn rdtsc_rdtsc() {
     println!("[rdtsc - rdtsc]");
 
     let mut total = 0;
@@ -16,17 +19,18 @@ fn main() {
 
         total += stop - start;
 
-        if print_values {
+        if PRINT_VALUES {
             println!("{} - {} = {}", start, stop, stop - start);
         }
-
-        std::thread::sleep(Duration::from_millis(SLEEP_DURATION));
     }
     println!("Average: {}", total / ITERATION_COUNT);
+}
 
+#[inline(always)]
+fn rdtsc_cpuid_rdtsc() {
     println!("[rdtsc - cpuid - rdtsc]");
 
-    total = 0;
+    let mut total = 0;
     for _ in 0..ITERATION_COUNT {
         let start = unsafe { _rdtsc() };
         let _ = unsafe { __cpuid(0) };
@@ -34,11 +38,49 @@ fn main() {
 
         total += stop - start;
 
-        if print_values {
+        if PRINT_VALUES {
             println!("{} - {} = {}", start, stop, stop - start);
         }
-
-        std::thread::sleep(Duration::from_millis(SLEEP_DURATION));
     }
     println!("Average: {}", total / ITERATION_COUNT);
+}
+
+/// See: https://secret.club/2020/01/12/battleye-hypervisor-detection.html
+#[inline(always)]
+fn rdtsc_cpuid_rdtsc_calibration() {
+    println!("[rdtsc - cpuid - rdtsc] (with calibration)");
+
+    let start = unsafe { _rdtsc() };
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    let timestamp_calibration = unsafe { _rdtsc() } - start;
+
+    let mut total = 0;
+    for _ in 0..0x6694 {
+        let start = unsafe { _rdtsc() };
+        let _ = unsafe { __cpuid(0) };
+        let stop = unsafe { _rdtsc() };
+
+        total += stop - start;
+
+        if PRINT_VALUES {
+            println!("{} - {} = {}", start, stop, stop - start);
+        }
+    }
+    let result = 10000000 * total / timestamp_calibration / 0x65;
+    println!("Average: {}", result);
+}
+
+fn main() {
+    let old_priority = unsafe { SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL as _) };
+
+    //
+    //
+
+    rdtsc_rdtsc();
+    rdtsc_cpuid_rdtsc();
+    rdtsc_cpuid_rdtsc_calibration();
+
+    //
+    //
+    unsafe { SetThreadPriority(GetCurrentThread(), old_priority) };
 }
